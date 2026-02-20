80876525070d7f2a1dabf95d078b1b0aa5e2121f
Roman <roman.demianovych@gmail.com>
2026-02-20 16:53:57 +0000

    Fixing errors

--- tests/test_assumptions.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from ua_batteries.assumptions import total_price, ROZPODIL, MARZHA, PEREDACHA
-
-def test_total_price_1():
-    assert total_price(0) == ROZPODIL + PEREDACHA
--- ua_batteries/__init__.py
+++ ua_batteries/__init__.py
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
 """UA Batteries energy optimization package."""
-=======
-# ua_batteries/__init__.py
-
-__version__ = "0.0.1"
->>>>>>> aedc33a734e48af1c64ced4cc6db287d126aabfd
